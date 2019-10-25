f18efb6e005447e44beb97b99bcfe6547e46648c
haeyden <micomecy@163.ccom>
2019-10-25 09:24:16 +0800

    springboot init demo

---
+++ demo/src/main/java/com/freesme/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.freesme.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
